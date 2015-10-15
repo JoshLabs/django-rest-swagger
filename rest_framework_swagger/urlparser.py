4789c598759254d4ba3c0b3394440628f41298c7
Arpit Goel <arpit.goel@joshtechnologygroup.com>
2015-10-15 13:34:01 +0530

    Fix removal of repeating resource name in the urls

--- rest_framework_swagger/urlparser.py
+++ rest_framework_swagger/urlparser.py
@@ -78,7 +78,7 @@ class UrlParser(object):
         filtered_paths = set()
         base_path = self.__get_base_path__(root_paths)
         for path in root_paths:
-            resource = path.replace(base_path, '').split('/')[0]
+            resource = path.replace(base_path, '', 1).split('/')[0]
             filtered_paths.add(base_path + resource)
 
         return list(filtered_paths)
